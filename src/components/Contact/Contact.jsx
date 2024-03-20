8ec0ab1f1dcd493ee3551c73ec9d85d942d60e29
Pratik Tomar <pratiksingh067@gmail.com>
2024-03-21 03:04:22 +0530

    some more mobile issue fixed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,41 +1,5 @@
-// import React from "react";
-
-// import styles from "./Contact.module.css";
-// import { getImageUrl } from "../../utils";
-
-// export const Contact = () => {
-//   return (
-//     <footer id="contact" className={styles.container}>
-//       <div className={styles.text}>
-//         <h2>Contact</h2>
-//         <p>Feel free to reach out!</p>
-//       </div>
-//       <ul className={styles.links}>
-//         <li className={styles.link}>
-//           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-//           <a href="mailto:myemail@email.com">myemail@email.com</a>
-//         </li>
-//         <li className={styles.link}>
-//           <img
-//             src={getImageUrl("contact/linkedinIcon.png")}
-//             alt="LinkedIn icon"
-//           />
-//           <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
-//         </li>
-//         <li className={styles.link}>
-//           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-//           <a href="https://www.github.com/myname">github.com/myname</a>
-//         </li>
-//       </ul>
-//     </footer>
-//   );
-// };
-
-// Contact.js
-
 import React from "react";
 import styles from "./Contact.module.css";
-// import { getImageUrl } from "../../utils";
 import emailIcon from "../../assets/contact/emailIcon.png";
 import linkedinIcon from "../../assets/contact/linkedinIcon.png";
 import githubIcon from "../../assets/contact/githubIcon.png";
